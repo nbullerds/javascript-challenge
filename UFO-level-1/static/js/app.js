b51ed5cbabe73be8ad91cd13f94dbf98bafeec6e
nabuller <nabuller@gmail.com>
2021-04-12 08:22:20 -0500

    began entering d3 code, no milestone reached

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,11 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(data);
+
+var tbody = d3.select("tbody");
+
+//Create a row for each instance of data in data.js
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+});
