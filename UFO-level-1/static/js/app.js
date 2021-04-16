444c20b4d5bcdc7abe0c76d231156a4f62d1e37b
unknown <nabuller@gmail.com>
2021-04-16 15:41:25 -0500

    Completed level 1 js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,34 @@
 var tableData = data;
 
-console.log(data);
-
-var tbody = d3.select("tbody");
-
 //Create a row for each instance of data in data.js
-data.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
+function updateTable(data) {
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    data.forEach(ufoSighting => {
     var row = tbody.append("tr");
-});
+
+        //For each data element appened table data to the current table row
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+function filterSightings() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // filter data based on data input and display to console
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    updateTable(filteredData);
+}
+
+updateTable(data);
+d3.select("#filter-btn").on("click", filterSightings);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,34 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//Create a row for each instance of data in data.js
+function updateTable(data) {
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    data.forEach(ufoSighting => {
+    var row = tbody.append("tr");
+
+        //For each data element appened table data to the current table row
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+function filterSightings() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // filter data based on data input and display to console
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    updateTable(filteredData);
+}
+
+updateTable(data);
+d3.select("#filter-btn").on("click", filterSightings);
